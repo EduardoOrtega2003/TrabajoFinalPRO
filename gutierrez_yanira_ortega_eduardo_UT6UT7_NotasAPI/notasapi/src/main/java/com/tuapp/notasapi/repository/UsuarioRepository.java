93d9df85ec8bbcabf5265781cacd4dd2b98f4698
EduOrtegaG <131004238+EduardoOrtega2003@users.noreply.github.com>
2025-06-08 20:59:04 +0100

    Branch Repository Service Eduardo
    He añadido tanto el repository como el service

---
+++ gutierrez_yanira_ortega_eduardo_UT6UT7_NotasAPI/notasapi/src/main/java/com/tuapp/notasapi/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.tuapp.notasapi.repository;
+
+import com.tuapp.notasapi.model.Usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    boolean existsByEmail(String email);
+}
