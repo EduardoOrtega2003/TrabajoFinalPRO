e65b18194ee6b1b35bac6cbe1a8abbdca62eb78c
EduOrtegaG <131004238+EduardoOrtega2003@users.noreply.github.com>
2025-06-08 19:59:07 +0100

    Delete gutierrez_yanira_ortega_eduardo_UT6UT7_NotasAPI/notasapi directory

--- gutierrez_yanira_ortega_eduardo_UT6UT7_NotasAPI/notasapi/src/main/java/com/tuapp/notasapi/service/NotaService.java
+++
@@ -1,8 +0,0 @@
-package com.tuapp.notasapi.service;
-
-import com.tuapp.notasapi.model.Nota;
-import java.util.List;
-
-public interface NotaService extends CrudService<Nota, Long> {
-    List<Nota> findByUsuarioId(Long usuarioId, String order);
-}
